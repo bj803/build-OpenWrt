dcd9a1f4c982f38acf1ee7547da1d066c735f092
bj803 <109253859+bj803@users.noreply.github.com>
2024-09-18 22:10:52 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,7 +8,16 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
+./scripts/feeds install -a 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
