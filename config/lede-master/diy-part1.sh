ae1936736486f83ee4985b44c7359d5cc8a92984
bj803 <109253859+bj803@users.noreply.github.com>
2024-09-19 13:59:08 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -17,7 +17,7 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-./scripts/feeds install -a 
+#./scripts/feeds install -a 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
